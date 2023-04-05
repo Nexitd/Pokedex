2800b3101d775eaf16f69b1a02d8bb1b69699f17
Nexitd <kern45061@gmail.com>
2023-04-05 12:21:01 +0300

    change vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    server: {
-        proxy: {
-            '^/api/v2': 'https://pokeapi.co/',
-        },
-    },
+   
 });
